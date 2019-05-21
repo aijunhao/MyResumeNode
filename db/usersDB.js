1b0712421d2be2aac50dca9085676de58c574da9
aijunhao <1172949809@qq.com>
2019-05-21 21:52:21 +0800

    设置 User_Name 的默认值

--- db/usersDB.js
+++ db/usersDB.js
@@ -1,6 +1,10 @@
 const db = require('./dbConfig.js')
 
-// 用户注册
+/**
+ * 用户注册
+ * Email, Password 为非空属性
+ * User_Name 属性默认值为 Email 值
+ */
 exports.register = (params, callback) => {
   db.query({
     sql: 'SELECT * FROM Users WHERE Email = ?',
@@ -9,8 +13,8 @@ exports.register = (params, callback) => {
     .then(results => {
       if (results.length === 0) {
         db.query({
-          sql: 'INSERT INTO Users(Email, Password) values(?, ?)',
-          values: [params.email, params.pass]
+          sql: 'INSERT INTO Users(Email, Password, User_Name) values(?, ?, ?)',
+          values: [params.email, params.pass, params.email]
         }).then(results => {
           return callback(null, results)
         })
@@ -28,9 +32,11 @@ exports.login = (params, callback) => {
   db.query({
     sql: 'SELECT User_Id, User_Name FROM Users WHERE Email = ?',
     values: [params.email]
-  }).then(results => {
-    return callback(null, results)
-  }).catch(err => {
-    return callback(err)
   })
+    .then(results => {
+      return callback(null, results)
+    })
+    .catch(err => {
+      return callback(err)
+    })
 }
